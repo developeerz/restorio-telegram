83470b7c70115296554b10b67ecbbadb73efa8b5
mikhail <mihaelvozhzhov@gmail.com>
2025-04-04 18:55:17 +0700

    Явное получение настроек postgres

--- config/config.go
+++ config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -16,8 +17,17 @@ var ConfigService Config
 func LoadConfig() {
 	godotenv.Load()
 
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+
 	ConfigService = Config{
-		Postgres: os.Getenv("POSTGRES_CONFIG"),
+		Postgres: dsn,
 		BotToken: os.Getenv("BOT_TOKEN"),
 	}
 }
