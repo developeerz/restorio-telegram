22378d7be2d240380bb73bb1f098539db4194f1b
mikhail <mihaelvozhzhov@gmail.com>
2025-05-12 17:23:18 +0700

    TDM-136 Добавил кафку

--- config/config.go
+++ config/config.go
@@ -1,27 +1,61 @@
 package config
 
 import (
+	"fmt"
 	"os"
+
+	"slices"
 
 	"github.com/joho/godotenv"
 	"github.com/rs/zerolog/log"
 )
 
 type Config struct {
-	BotToken    string
-	ServiceName string
+	botToken    string
+	serviceName string
+	brokers     []string
+	topic       string
 }
 
-var ConfigService Config
+func (c *Config) BotToken() string {
+	return c.botToken
+}
 
-func LoadConfig() {
+func (c *Config) ServiceName() string {
+	return c.serviceName
+}
+
+func (c *Config) Brokers() []string {
+	return c.brokers
+}
+
+func (c *Config) Topic() string {
+	return c.topic
+}
+
+func LoadConfig() (*Config, error) {
 	err := godotenv.Load()
 	if err != nil {
 		log.Info().Msgf("load dotenv: %v", err)
 	}
 
-	ConfigService = Config{
-		BotToken:    os.Getenv("BOT_TOKEN"),
-		ServiceName: os.Getenv("SERVICE_NAME"),
+	botToken := os.Getenv("BOT_TOKEN")
+	serviceName := os.Getenv("SERVICE_NAME")
+	broker := os.Getenv("BROKERS")
+	topic := os.Getenv("TOPIC")
+
+	if anyIsEmpty(botToken, serviceName, broker, topic) {
+		return nil, fmt.Errorf("empty property")
 	}
+
+	return &Config{
+		botToken:    botToken,
+		serviceName: serviceName,
+		brokers:     []string{broker},
+		topic:       topic,
+	}, nil
+}
+
+func anyIsEmpty(properties ...string) bool {
+	return slices.Contains(properties, "")
 }
--- go.mod
+++ go.mod
@@ -13,9 +13,13 @@ require (
 
 require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/developeerz/restorio-reserving v0.0.0-20250509061834-383ab97e4864
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/klauspost/compress v1.15.9 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/pierrec/lz4/v4 v4.1.15 // indirect
 	github.com/redis/go-redis/v9 v9.7.3 // indirect
+	github.com/segmentio/kafka-go v0.4.47 // indirect
 	golang.org/x/sys v0.32.0 // indirect
 )
